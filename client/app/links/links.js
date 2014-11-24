9e6af2fabcf9f95874dcac1012690cc1b1511929
chiaS <pretend15@gmail.com>
2014-11-24 15:43:25 -0800

    progress

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,14 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {
+    links: []
+  };
+  $scope.getLinks = function(links){
+    return $scope.data.links;
+  };
+  $scope.addLink = function(link) {
+    $scope.data.links.push(link);
+  };
 });
+
